2aebfdb01ecded8ee97389ff1bd6d8fb9e2ff46b
verolence <verolence@yandex.ru>
2026-02-12 19:59:13 +0300

    add logging config

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.database import init_db
+from app.logging_config import setup_logging
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging()
     init_db()
     yield
 
