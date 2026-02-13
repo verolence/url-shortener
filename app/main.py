49f023ba0ddfdef345f0c9e848ded84f61deee46
verolence <verolence@yandex.ru>
2026-02-13 13:44:28 +0300

    add get code endpoint

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from app.database import init_db
 from app.logging_config import setup_logging
 from pydantic import BaseModel
 from app.service import generate_code, is_valid_url
-from app.repository import insert_url
+from app.repository import insert_url, get_url_by_code
+import logging
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     setup_logging()
     init_db()
     yield
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI(lifespan=lifespan)
 
@@ -28,3 +32,13 @@ def shorten_url(item: URLItem):
     insert_url(code, item.url)
 
     return {"short_url": f"{BASE_URL}/{code}"}
+
+@app.get("/{code}")
+def redirect_to_url(code: str):
+    original_url = get_url_by_code(code)
+    if original_url:
+        logger.info(f"Redirecting code {code} to {original_url}")
+        return RedirectResponse(url=original_url, status_code=307)
+    else:
+        logger.warning(f"Code {code} not found")
+        raise HTTPException(status_code=404, detail="URL not found")
