a79baa5a3a5f3acc973e6e2b88f3781e4e5e2805
verolence <verolence@yandex.ru>
2026-02-13 19:49:14 +0300

    add tests that partly pass

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,48 @@
+import pytest
+from fastapi.testclient import TestClient
 from pathlib import Path
-
+from app.main import app
+from app.database import init_db
+from app.repository import insert_url, get_url_by_code
+from app.service import generate_code
 
 TEST_DB = Path("test.db")
+
+@pytest.fixture(scope="module")
+def client():
+    if TEST_DB.exists():
+        TEST_DB.unlink()
+    init_db(db_path=TEST_DB)
+
+    yield TestClient(app)
+
+    if TEST_DB.exists():
+        TEST_DB.unlink()
+
+def test_create_shorten_url(client):
+    response = client.post("/shorten", json={"url": "https://example.com"})
+    assert response.status_code == 201
+    data = response.json()
+    assert "short_url" in data
+    assert data["short_url"].startswith("http://localhost:8000/")
+    code = data["short_url"].split("/")[-1]
+    original = get_url_by_code(code, db_path=TEST_DB)
+    assert original == "https://example.com"
+
+def test_invalid_url(client):
+    response = client.post("/shorten", json={"url": "invalid-url"})
+    assert response.status_code == 400
+    assert "Invalid URL" in response.json()["detail"]
+
+def test_redirect(client):
+    code = generate_code()
+    insert_url(code, "https://example.com", db_path=TEST_DB)
+    response = client.get(f"/{code}", follow_redirects=False)
+    assert response.status_code == 307
+    assert response.headers["location"] == "https://example.com"
+
+def test_404_redirect(client):
+    """404 если код не найден"""
+    response = client.get("/nonexistentcode", follow_redirects=False)
+    assert response.status_code == 404
+    assert response.json()["detail"] == "URL not found"
